9ac2fc9ab61948ff5647927515160e4f3c548e5a
Ankitmahajna022 <ankitmahajan2257@gmail.com>
2024-06-25 11:53:34 +0530

    Add files via upload

---
+++ c progaram/PR1/Q1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  float celsius,faherenheit,remainingCelsius;
+  
+  printf("Enter celsius:");
+  scanf("%f",&celsius);
+  
+  faherenheit=(1.8*celsius)+32;
+  
+  
+  
+  printf("fahernheit=%.2f",faherenheit);
+  
+  
+
+}
+
