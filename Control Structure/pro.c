1ad4b3e6ac6139888ab018f91a88ab9dcd779373
Ankitmahajna022 <ankitmahajan2257@gmail.com>
2024-07-01 11:30:56 +0530

    Add files via upload

---
+++ Control Structure/pro.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+ {
+int x,y;
+   
+    printf("Enter the value x :");
+    scanf("%d",&x);
+ 
+    printf("Enter the value y :");
+    scanf("%d",&y);
+
+    (x>y)?printf("x maxmum"):printf("y maxmum");
+}
