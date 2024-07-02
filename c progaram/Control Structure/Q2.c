23b8951dc21e7817f4e8ec95e3566550e6a42a56
Ankitmahajna022 <ankitmahajan2257@gmail.com>
2024-07-02 12:17:01 +0530

    Update Q2.c

--- c progaram/Control Structure/Q2.c
+++ c progaram/Control Structure/Q2.c
@@ -5,7 +5,7 @@ void main()
 {
 	int choice;
 	
-	printf("Press 1 for English\n");
+    printf("Press 1 for English\n");
     printf("Press 2 for Hindi\n");
     printf("Press 3 for Gujarati\n");
     
@@ -15,12 +15,12 @@ void main()
     switch(choice)
     {
     	case 1:
-    		printf("Press 1 for Internet Recharge\n");
+    	    printf("Press 1 for Internet Recharge\n");
             printf("Press 2 for Top-up Recharge\n");
             printf("Press 3 for Special Recharge\n");
             
              printf("Enter Your choice:"); 
-            scanf("%d",&choice);
+             scanf("%d",&choice);
             switch(choice)
             {
             	case 1:printf("You have successfully done a Internet Recharge!");
@@ -34,12 +34,12 @@ void main()
 			}
     	break;
     	case 2:
-    		printf("Internet Recharge ke liye 1 dabaiye\n");
+    	    printf("Internet Recharge ke liye 1 dabaiye\n");
             printf("Top-up Recharge ke liye 2 dabaiye\n");
             printf("Special Recharge ke liye 3 dabaiye\n");
             
              printf("Enter Your choice:"); 
-            scanf("%d",&choice);
+             scanf("%d",&choice);
             switch(choice)
             {
             	case 1:printf("Aapne safaltapurvak Internet Recharge kar liya he!");
@@ -54,11 +54,11 @@ void main()
     	break;
     	case 3:
     		printf("Internet Recharge mate 1 dabavo\n");
-            printf("Top-up Recharge mate 2 dabavo\n");
-            printf("Special Recharge mate 3 dabavo\n");
+                printf("Top-up Recharge mate 2 dabavo\n");
+                printf("Special Recharge mate 3 dabavo\n");
             
              printf("Enter Your choice:"); 
-            scanf("%d",&choice);
+             scanf("%d",&choice);
             switch(choice)
             {
             	case 1:printf("Tame safaltapurvak Internet Recharge karyu chhe!");
