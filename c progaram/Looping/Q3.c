d34c12bc618e9fa4d864703c4477091f763aaf35
Ankitmahajna022 <ankitmahajan2257@gmail.com>
2024-07-25 11:50:15 +0530

    Add files via upload

---
+++ c progaram/Looping/Q3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+	int a=1;
+	int b;
+	
+	printf("Enter b value :");
+	scanf("%d",&b);
+	
+	while(a<=10)
+	{
+		printf("%d",a);
+		a++;
+	}
+	
